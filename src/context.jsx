1969a7457800c8f6b3a3f763a2e312d47ade0150
Harshwardhan Koushik <harshwardhankoushik@gmail.com>
2023-05-31 01:13:57 +0530

    v1 for sidebar app

---
+++ src/context.jsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useEffect, useState } from "react"
+
+const AppContext = createContext()
+
+export const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const openSidebar = () => {
+        
+        setIsSidebarOpen(true)
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
+    useEffect(() => {
+        console.log(isSidebarOpen, isModalOpen)
+    }, [isSidebarOpen, isModalOpen])
+
+    return (
+        <AppContext.Provider 
+            value={{
+                isSidebarOpen,
+                isModalOpen,
+                openModal,
+                openSidebar,
+                closeModal,
+                closeSidebar,
+            }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
